ae12bb0cd3215f66f85681343c3ba40d96caa202
Luca <luca.ant96@libero.it>
2020-07-26 16:49:18 +0200

    update

--- script/run_polybar.sh
+++ script/run_polybar.sh
@@ -6,10 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-#polybar primary &
-
 PRIMARY=$(xrandr -q | grep primary | cut -d' ' -f1)
+
+# Launch Polybar, using default config location ~/.config/polybar/config
 
 for m in $(xrandr -q | grep " connected" | cut -d" " -f1); do
     if [ "$m" = "$PRIMARY" ] ; then
