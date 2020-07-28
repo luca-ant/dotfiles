c49727c45656fd1b25f19bbb700d026afbfa6192
Luca <luca.ant96@libero.it>
2020-07-28 17:00:44 +0200

    update

--- script/weather.sh
+++ script/weather.sh
@@ -1,20 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 T=0
 
 weather() {
+    FORMAT=3
+#    FORMAT=4
+#    FORMAT="%l:+%c+🌡️ %t+🌫️ %h+🌬️ %w+🌧️ %o"
+#    FORMAT="%25l:+%25c+%F0%9F%8C%A1%EF%B8%8F%25t+%F0%9F%8C%AB%EF%B8%8F%25h+%F0%9F%8C%AC%EF%B8%8F%25w+%F0%9F%8C%A7%EF%B8%8F%25o%0A"
+
+    if ! ping -c1 wttr.in > /dev/null 2>&1
+    then
+        echo ""
+        return
+    fi
     if [ $T -eq 0 ]; then
-        curl -s "wttr.in?format=4"
+        W=$(curl -s "wttr.in?format=$FORMAT")
+        echo $W
     elif [ $T -eq 1 ]; then
-        curl -s "wttr.in/San Lorenzo in Campo?format=4"
+        W=$(curl -s "wttr.in/San Lorenzo in Campo?format=$FORMAT")
+        echo $W
     else
-        curl -s "wttr.in/Bologna?format=4"
+        W=$(curl -s "wttr.in/Bologna?format=$FORMAT")
+        echo $W
     fi
 }
 
 toggle() {
     T=$(((T + 1) % 3))
-    kill -15 $(pgrep --oldest --parent $$)
+    kill -15 $(pgrep --oldest --parent $$) > /dev/null 2>&1
 }
 
 trap "toggle" USR1
