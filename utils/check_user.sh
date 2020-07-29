8e85d7332604e93a9c8ef5041bc35797ee09e5ff
Luca <luca.ant96@libero.it>
2020-07-29 11:00:25 +0200

    update

--- utils/check_user.sh
+++ utils/check_user.sh
@@ -4,7 +4,7 @@ BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 
 if [ $EUID == 0 ] ; then
-    echo "${BOLD}You are root! Please, run me as a normal user.${NORMAL}"
+    echo "${BOLD}You are root! Please, run me as normal user.${NORMAL}"
     exit 1
 else
     echo "${BOLD}Hi, $USER${NORMAL}"
