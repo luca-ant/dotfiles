bec594f0a2c7ae77590d95a27cf65c27c0c0cd35
Luca <luca.ant96@libero.it>
2020-09-05 13:41:01 +0200

    minor changes

--- script/weather.sh
+++ script/weather.sh
@@ -2,11 +2,22 @@
 
 T=0
 
+output(){
+    W="$1"
+    if [ -z "$W" ]
+    then
+        echo "Please, look outside..."
+    else
+        echo "$W"
+    fi
+}
+
+
 weather() {
     FORMAT=3
-#    FORMAT=4
-#    FORMAT="%l:+%c+🌡️ %t+🌫️ %h+🌬️ %w+🌧️ %o"
-#    FORMAT="%25l:+%25c+%F0%9F%8C%A1%EF%B8%8F%25t+%F0%9F%8C%AB%EF%B8%8F%25h+%F0%9F%8C%AC%EF%B8%8F%25w+%F0%9F%8C%A7%EF%B8%8F%25o%0A"
+    #    FORMAT=4
+    #    FORMAT="%l:+%c+🌡️ %t+🌫️ %h+🌬️ %w+🌧️ %o"
+    #    FORMAT="%25l:+%25c+%F0%9F%8C%A1%EF%B8%8F%25t+%F0%9F%8C%AB%EF%B8%8F%25h+%F0%9F%8C%AC%EF%B8%8F%25w+%F0%9F%8C%A7%EF%B8%8F%25o%0A"
 
     if ! ping -c1 wttr.in > /dev/null 2>&1
     then
@@ -14,14 +25,19 @@ weather() {
         return
     fi
     if [ $T -eq 0 ]; then
-        W=$(curl -s "wttr.in?format=$FORMAT")
-        echo $W
+        W=$(curl -s "wttr.in?format=$FORMAT" 2>/dev/null | grep -v "Unknown location")
+
+        output "$W"
+
     elif [ $T -eq 1 ]; then
-        W=$(curl -s "wttr.in/San Lorenzo in Campo?format=$FORMAT")
-        echo $W
+        W=$(curl -s "wttr.in/San Lorenzo in Campo?format=$FORMAT" 2>/dev/null | grep -v "Unknown location")
+
+        output "$W"
+
     else
-        W=$(curl -s "wttr.in/Bologna?format=$FORMAT")
-        echo $W
+        W=$(curl -s "wttr.in/Bologna?format=$FORMAT" 2>/dev/null | grep -v "Unknown location")
+
+        output "$W"
     fi
 }
 
@@ -36,7 +52,7 @@ echo $$ >> /tmp/weather_pid
 
 while true; do
     weather
-    sleep 3600 &
+    sleep 1800 &
     wait
 done
 
