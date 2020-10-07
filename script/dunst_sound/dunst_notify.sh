b38cfea520a82d661d6351ed39f2d783330536e0
Luca <luca.ant96@libero.it>
2020-10-07 20:26:50 +0200

    minor changes

---
+++ script/dunst_sound/dunst_notify.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+WD=$(dirname "$(realpath "$0")")
+paplay "$WD/crystal_drop.ogg"
---
+++ script/workstation.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+#move this windows to ws9
+i3-msg move container to workspace 9
+
+for i in {1..8}
+do
+    i3-input -F "[workspace=$i] kill" -P "kill workspace $i"
+done
+
+# terminal
+i3-msg "workspace 1; exec $TERMINAL"
+sleep 1
+
+# browser
+i3-msg "workspace 2; exec $BROWSER"
+sleep 1
+i3-msg "workspace 3; exec $BROWSER"
+sleep 1
+
+# visual studio code
+#i3-msg "workspace 3; exec code"
+#sleep 1
+
+# browser
+i3-msg "workspace 4; exec $BROWSER"
+sleep 1
+
+# terminal
+i3-msg "workspace 5; exec $TERMINAL"
+sleep 1
+i3-msg "workspace 6; exec $TERMINAL"
+sleep 1
+
+# spotify
+i3-msg "workspace 8; exec spotify"
+sleep 1
+
