6255c28c864328ef3310c6000d0e61d0f9edf760
Luca <luca.ant96@libero.it>
2020-10-23 14:54:06 +0200

    minor changes

--- script/battery_monitor.sh
+++ script/battery_monitor.sh
@@ -19,7 +19,7 @@ checkBatteryLevel() {
 
     if [ "$battery_state" != "Discharging" ] || [ "${battery_level}" == "${previous_battery_level}" ]; then
         exit
-        notify-send "Battery (${battery_level}%)"
+        notify-send "Battery (${battery_level}%)" -u low -t 10000
     fi
 
     if [ $battery_level -le 7 ]; then
@@ -32,29 +32,29 @@ checkBatteryLevel() {
 #        notify-send "Very Low Battery (${battery_level}%)" "${battery_remaining} of battery remaining.\nYour computer will suspend soon unless plugged into a power source!" -u critical -t 30000
 
     elif [ $battery_level -le 15 ]; then
-        notify-send "Low Battery (${battery_level}%)" "${battery_remaining} of battery remaining." -u normal
+        notify-send "Low Battery (${battery_level}%)" "${battery_remaining} of battery remaining." -u normal -t 30000
     fi
 }
 
 checkBatteryStateChange() {
 
     if [ "$battery_state" == "Full" ] && [ "$previous_battery_state" == "Charging" ]; then
-        notify-send "Battery Charged" -u low
+        notify-send "Battery Charged" -u low -t 10000
     fi
 
     if [ "$battery_state" == "Charging" ] && [ "$previous_battery_state" == "Discharging" ]; then
-        notify-send "Charging" "Battery is now plugged in." -u low
+        notify-send "Charging" "Battery is now plugged in." -u low -t 10000
     fi
 
 #    if [ "$battery_state" == "Charging" ] && [ "$previous_battery_state" != "Charging" ]; then
-#        notify-send "Charging" "Battery is now plugged in." -u low
+#        notify-send "Charging" "Battery is now plugged in." -u low -t 10000
 #    fi
 
     if [ "$battery_state" == "Discharging" ] && [ "$previous_battery_state" == "Charging" ]; then
-        notify-send "Power Unplugged" "Your computer has been disconnected from power." -u low
+        notify-send "Power Unplugged" "Your computer has been disconnected from power." -u low -t 10000
     fi
     if [ "$battery_state" == "Discharging" ] && [ "$previous_battery_state" == "Full" ]; then
-        notify-send "Power Unplugged" "Your computer has been disconnected from power." -u low
+        notify-send "Power Unplugged" "Your computer has been disconnected from power." -u low -t 10000
     fi
 }
 
