a08842b050e18c1131b64c43cd6a5f0846c7bb32
Luca <luca.ant96@libero.it>
2020-11-13 20:06:48 +0100

    fix polibar mirror screen

--- script/run_polybar.sh
+++ script/run_polybar.sh
@@ -15,14 +15,23 @@ export WLAN_INTERFACE=$(cat /proc/net/dev | grep -e '^wl' | cut -d: -f1 | head -
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 
-for m in $(xrandr -q | grep " connected" | cut -d" " -f1); do
-    if [ "$m" = "$PRIMARY" ] ; then
-        MONITOR=$m polybar bottom >/dev/null 2>&1 &
-        MONITOR=$m polybar top >/dev/null 2>&1 &
-        echo
-    else
-        MONITOR=$m polybar bottom2 >/dev/null 2>&1 &
-#        MONITOR=$m polybar top2 >/dev/null 2>&1 &
-        echo
-    fi
-done
+if [[ $(polybar -m | wc -l) == 1 && $(xrandr -q | grep " connected" | wc -l) > 1 ]]
+then
+    EXTEND=false MONITOR=$m polybar bottom >/dev/null 2>&1 &
+    EXTEND=false MONITOR=$m polybar top >/dev/null 2>&1 &
+
+else
+
+    for m in $(xrandr -q | grep " connected" | cut -d" " -f1); do
+        if [ "$m" = "$PRIMARY" ] ; then
+            MONITOR=$m polybar bottom >/dev/null 2>&1 &
+            MONITOR=$m polybar top >/dev/null 2>&1 &
+            echo
+        else
+            MONITOR=$m polybar bottom2 >/dev/null 2>&1 &
+#            MONITOR=$m polybar top2 >/dev/null 2>&1 &
+            echo
+        fi
+    done
+
+fi
