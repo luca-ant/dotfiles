8f136172dcd625b894751afb231f0095d48415c4
Luca <luca.ant96@libero.it>
2021-08-02 21:35:32 +0200

    update

--- custom/run_polybar.sh
+++ custom/run_polybar.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+COMMAND=''
+
+if which polybar > /dev/null 2>&1
+then
+    COMMAND='polybar'
+    CFG='-c ~/.config/polybar/config'
+elif which polybar-git > /dev/null 2>&1
+then
+    COMMAND='polybar-git'
+    CFG='-c ~/.config/polybar/config'
+fi
+
+if [ -z "$COMMAND" ]
+then
+    echo "polybar not found" >&2
+    exit 1
+fi
+
 # Terminate already running bar instances
-killall -q polybar
+killall -q $COMMAND
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x $COMMAND >/dev/null; do sleep 1; done
 
 rm /tmp/.weather_pid >/dev/null 2>&1
 rm /tmp/.outputaudio_pid >/dev/null 2>&1
@@ -16,22 +34,22 @@ export WLAN_INTERFACE=$(cat /proc/net/dev | grep -e '^wl' | cut -d: -f1 | head -
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 
-if [[ $(polybar -m | wc -l) == 1 && $(xrandr -q | grep " connected" | wc -l) > 1 ]]
+if [[ $($COMMAND -m | wc -l) == 1 && $(xrandr -q | grep " connected" | wc -l) > 1 ]]
 then
-    m=$(polybar -m | cut -d: -f1)
-    EXTEND=false MONITOR=$m polybar bottom >/dev/null 2>&1 &
-    EXTEND=false MONITOR=$m polybar top >/dev/null 2>&1 &
+    m=$(polybar-git -m | cut -d: -f1)
+    EXTEND=false MONITOR=$m $COMMAND $CFG bottom >/dev/null 2>&1 &
+    EXTEND=false MONITOR=$m $COMMAND $CFG top >/dev/null 2>&1 &
 
 else
 
     for m in $(xrandr -q | grep " connected" | cut -d" " -f1); do
         if [ "$m" = "$PRIMARY" ] ; then
-            MONITOR=$m polybar bottom >/dev/null 2>&1 &
-            MONITOR=$m polybar top >/dev/null 2>&1 &
+            MONITOR=$m $COMMAND $CFG bottom >/dev/null 2>&1 &
+            MONITOR=$m $COMMAND $CFG top >/dev/null 2>&1 &
             echo
         else
-            MONITOR=$m polybar bottom2 >/dev/null 2>&1 &
-#            MONITOR=$m polybar top2 >/dev/null 2>&1 &
+            MONITOR=$m $COMMAND $CFG bottom2 >/dev/null 2>&1 &
+#            MONITOR=$m $COMMAND $CFG top2 >/dev/null 2>&1 &
             echo
         fi
     done
