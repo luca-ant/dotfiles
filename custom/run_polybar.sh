4b973dc2c788557c7d69dfafb5ebe4ee1b9ea4d2
Luca <luca.ant96@libero.it>
2021-08-29 16:40:28 +0200

    minor changes

--- custom/run_polybar.sh
+++ custom/run_polybar.sh
@@ -5,11 +5,7 @@ COMMAND=''
 if which polybar > /dev/null 2>&1
 then
     COMMAND='polybar'
-    CFG='-c ~/.config/polybar/config'
-elif which polybar-git > /dev/null 2>&1
-then
-    COMMAND='polybar-git'
-    CFG='-c ~/.config/polybar/config'
+    CFG='~/.config/polybar/config'
 fi
 
 if [ -z "$COMMAND" ]
@@ -37,19 +33,19 @@ export WLAN_INTERFACE=$(cat /proc/net/dev | grep -e '^wl' | cut -d: -f1 | head -
 if [[ $($COMMAND -m | wc -l) == 1 && $(xrandr -q | grep " connected" | wc -l) > 1 ]]
 then
     m=$($COMMAND -m | cut -d: -f1)
-    EXTEND=false MONITOR=$m $COMMAND $CFG bottom >/dev/null 2>&1 &
-    EXTEND=false MONITOR=$m $COMMAND $CFG top >/dev/null 2>&1 &
+    EXTEND=false MONITOR=$m $COMMAND -c "$CFG" bottom >/dev/null 2>&1 &
+    EXTEND=false MONITOR=$m $COMMAND -c "$CFG" top >/dev/null 2>&1 &
 
 else
 
     for m in $(xrandr -q | grep " connected" | cut -d" " -f1); do
         if [ "$m" = "$PRIMARY" ] ; then
-            MONITOR=$m $COMMAND -c ~/.config/polybar/config bottom >/dev/null 2>&1 &
-            MONITOR=$m $COMMAND -c ~/.config/polybar/config top >/dev/null 2>&1 &
+            MONITOR=$m $COMMAND -c "$CFG" bottom >/dev/null 2>&1 &
+            MONITOR=$m $COMMAND -c "$CFG" top >/dev/null 2>&1 &
             echo
         else
-            MONITOR=$m $COMMAND -c ~/.config/polybar/config bottom2 >/dev/null 2>&1 &
-#            MONITOR=$m $COMMAND -c ~/.config/polybar/config$CFG top2 >/dev/null 2>&1 &
+            MONITOR=$m $COMMAND -c "$CFG" bottom2 >/dev/null 2>&1 &
+#            MONITOR=$m $COMMAND -c "$CFG" top2 >/dev/null 2>&1 &
             echo
         fi
     done
